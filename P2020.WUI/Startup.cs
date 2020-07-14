576dbcfddc7e297277c2e9af1ebda702262dfc0b
Tanvi Desai <dtanvi1803@yahoo.co.in>
2020-07-14 14:56:21 +0530

    Project Create .Net Framework 4.5

---
+++ P2020.WUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(P2020.WUI.Startup))]
+namespace P2020.WUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
